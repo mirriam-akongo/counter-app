4ae01b74f07f70443a4875837f666a4b578e5907
mirriam-akongo <jemmirriako@gmail.com>
2020-05-29 19:25:08 +0300

    Added increment functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,17 +3,38 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <img src={this.state.imageUrl} alt="" m-4 />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
